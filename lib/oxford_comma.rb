e8ed56cf2ffe4e2ed82fd866bb9ac9e455349fa5
Larissa S <larissafschiavo@gmail.com>
2017-11-19 05:29:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,11 @@ def oxford_comma(array)
   ans = ""
   if array.length == 1
     ans << array[0]
-  end 
+  elsif array.length == 2
+    ans << array[0] + " and " + array[1]
+  elsif array.length > 2
+    array.each_with_index do |item, idx|
+      if idx != array.length -1
+        ans << item + ", "
+  return ans
 end
